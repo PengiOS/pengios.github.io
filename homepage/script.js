c02eb1e25e2ed3d9031a263003a09bd0fbd89a5a
MrPiggy105 <100971521+MrPiggy105@users.noreply.github.com>
2024-09-20 16:32:34 +1000

    Implement custom clock (TODO: Date)
    Also it's annoying because font isn't monospaced so the text shifts for thinner characters

--- homepage/script.js
+++ homepage/script.js
@@ -36,9 +36,21 @@ document.addEventListener('DOMContentLoaded', () => {  // ensure all the DOM con
   var weatherDetails4;
 
   function updateTime() {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
+    let date = new Date();
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let seconds = date.getSeconds();
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
+
     var greetingText;
       
     if (hours >= 0 && hours < 3) {
