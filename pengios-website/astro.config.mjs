0fc3dc1a5d7cb5e665a8a5a6cf434e9e61d07e82
MrPiggy105 <100971521+MrPiggy105@users.noreply.github.com>
2024-09-25 14:17:04 +1000

    Finish porting to astro. Revert back to this if any problems. see desc
    Will fully merge next. I didn't do the home page or download page because there isn't anything on there

--- pengios-website/astro.config.mjs
+++ pengios-website/astro.config.mjs
@@ -1,9 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+    site: 'https://astro-article.netlify.app/',
+    integrations: [
+        mdx(), 
+        sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
 });
